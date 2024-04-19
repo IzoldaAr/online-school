5d14b32d1d3f2e7c2a24ce4d4f9b20a95aeeb917
IzoldaAr <izolda.arshakyan@betconstruct.com>
2024-04-19 14:29:48 +0400

    css config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,26 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      //Creates 'style' nodes from JS strings,
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      //Translates CSS into Common JS,
+      "css-loader",
+      //Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  //Если не используем тайпскрипт - нужен babel-loader
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
 
-    return [
-        typescriptLoader,
-    ]
+  return [typescriptLoader, cssLoader];
 }
